33650c941d752aa381cd61269648f66453210e65
Sergio Miguéns Iglesias <sergio@lony.xyz>
2022-09-14 17:54:00 +0200

    install/day2: Improve documentation

--- installation/day2.sh
+++ installation/day2.sh
@@ -3,26 +3,21 @@
 # Day2 installation scripts. Run this after the archinstall installer to
 # recreate my setup, data not included.
 #
+# USAGE:
+# curl https://raw.githubusercontent.com/lonyelon/scripts/main/installation/day2.sh | sh /dev/stdin USERNAME
+#
 # Made by SERGIO MIGUÉNS IGLESIAS <sergio@lony.xyz> for personal use, 2022.
 
 user=$1
 homedir=`getent passwd $user | cut -d: -f6`
 
-pacman -Syyu
+#                                                     INSTALLATION PREREQUISITES
+################################################################################
 
-packages='
-    vim
-    unison rsync
-    bc fzf
-    git
-    man-db man-pages
-    keepassxc sxiv
-    wget curl
-    hledger
-    task
-'
+pacman -Syyu fzf git rsync
 
-pacman -S $packages
+#                                                                     XORG SETUP
+################################################################################
 
 drivers='
     xf86-video-amdgpu
@@ -38,6 +33,9 @@ option=$(echo "$drivers" | grep '[a-z]' | tr -d ' ' \
 [ -z "$option" ] && option="xf86-video-fbdev"
 pacman -S xorg-server $option
 
+#                                                             SUCKLESS UTILITIES
+################################################################################
+
 git clone https://github.com/lonyelon/dwm.git /tmp/dwm
 git clone https://github.com/lonyelon/dmenu.git /tmp/dmenu
 git clone https://github.com/lonyelon/st.git /tmp/st
@@ -49,20 +47,53 @@ make install
 cd /tmp/st
 make install
 
+#                                                                          EMACS
+################################################################################
+
 pacman -S emacs
 #pacman -S ripgrep
 git clone --depth 1 https://github.com/doomemacs/doomemacs $homedir/.emacs.d
 $homedir/.emacs.d/bin/doom install
+
+#                                                                       OPENDOAS
+################################################################################
 
 pacman -S opendoas
 echo 'permit keepenv persist :wheel' > /etc/doas.conf
 pacman -Rns sudo || exit
 ln -s /bin/doas /bin/sudo
 
-git clone https://github.com/lonyelon/dwm.git $homedir/sh
+#                                                                            YAY
+################################################################################
 
 git clone https://aur.archlinux.org/yay-git.git /opt/yay-git
 cd /opt/yay-git
 makepkg -si
 
+#                                                                 CUSTOM SCRIPTS
+################################################################################
+
+git clone https://github.com/lonyelon/scripts.git $homedir/sh
+
+#                                                                      SSH SETUP
+################################################################################
+
 su $user -c ssh-keygen
+sed -i 's/^X11Forwarding yes/X11Forwarding no/'
+sed -i 's/^UsePAM yes/UsePam no/'
+
+#                                                    MISC UTILITIES INSTALLATION
+################################################################################
+
+packages='
+    vim
+    unison
+    bc
+    man-db man-pages
+    keepassxc sxiv
+    wget
+    hledger
+    task
+'
+
+pacman -S $packages
